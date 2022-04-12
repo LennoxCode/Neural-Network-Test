4e1a4ffdc52b9ff207f62a73a4e6f40c9e6ccf4a
LennoxCode <leonard.valentin1999@gmail.com>
2022-04-12 13:04:41 +0200

    added query functin to determine output of network

--- NN.py
+++ NN.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[19]:
 
 
 import numpy
+import scipy.special
 
 
-# In[8]:
+# In[29]:
 
 
 class neuralNetwork:
@@ -19,25 +20,37 @@ class neuralNetwork:
         #learning rate of the network
         self.lr = learning_rate
         #initializing weight matrix
-        self.wih = (numpy.random.rand(self.hnodes, self.inodes) - 0.5)
-        self.who = (numpy.random.rand(self.onodes, self.hnodes) - 0.5)
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+        self.activation_function = lambda x: scipy.special.expit(x)
         pass
     
     def train():
         pass
     
-    def query():
-        pass
+    def query(self, inputs_list):
+        #calculating the hidden layer
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        print(inputs)
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        return final_outputs
+        
+        
     
 
 
-# In[9]:
+# In[30]:
 
 
 n = neuralNetwork(3,3,3,0.3)
 print(n.inodes)
 print(n.wih)
 print(n.who)
+n.query([1.0, 0.5, -1.5])
 
 
 # In[ ]:
